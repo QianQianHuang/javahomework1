2d78c9db09fbd14a294c973139fb3f8d33eac6bf
QainQainHuang <edison90829@gmail.com>
2020-03-11 19:45:53 +0800

    class HomeWorkNew1

---
+++ HomeWorkNew1.java
@@ -0,0 +1,11 @@
+/**
+ * HomeWorkNew
+ */
+public class HomeWorkNew1 {
+
+    public static void main(String[] args) {
+        Book2 book1 = new Book2() ;
+        book1.showlnfo() ;
+       
+    }
+}
