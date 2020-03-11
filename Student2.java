54aec6b77b5aafff20275232aa86c692a8ca2942
QainQainHuang <edison90829@gmail.com>
2020-03-11 19:49:12 +0800

    class Student

---
+++ Student2.java
@@ -0,0 +1,21 @@
+/**
+ * Student2
+ */
+public class Student2 {
+
+    private String name , gender , studentid , department , age ;
+
+    public Student2()
+    {
+        System.out.println("Student object is created") ;
+    }
+
+    public void showlnfo()
+    {
+        System.out.println("The Student Name is QianQian Huang") ;
+        System.out.println("The Student Gender is Boy") ;
+        System.out.println("The Student Id is 108021081") ;
+        System.out.println("The Student Department is IT") ;
+        System.out.println("The Student Age is 18") ;
+    }
+}
