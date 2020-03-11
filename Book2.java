45f16525deabfed207ab1a79bcf649a1eae5ae7e
QainQainHuang <edison90829@gmail.com>
2020-03-11 19:40:30 +0800

    class Book

---
+++ Book2.java
@@ -0,0 +1,23 @@
+/**
+ * Book2
+ */
+public class Book2 {
+
+    private String name , author , kind , issuedate , pages ;
+
+    public Book2()
+    {
+        System.out.println("book object has created") ;
+    }
+
+    public void showlnfo()
+    {    
+       System.out.println("The Book Name is NoName") ;
+       System.out.println("The Author is QianQian Huang") ;
+       System.out.println("The Book kind is Romance") ;
+       System.out.println("The Book Issuedate is 2020/3/11") ;
+       System.out.println("The Book Pages is 200") ;
+        
+    }
+    
+}
