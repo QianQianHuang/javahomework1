c9b98a9d8dbdb4c7de5fd7140fb53272d1c23789
QainQainHuang <edison90829@gmail.com>
2020-03-13 20:07:53 +0800

    class  Homework2

---
+++ Homework2.java
@@ -0,0 +1,11 @@
+/**
+ * Homework2
+ */
+public class Homework2 {
+
+    public static void main(String[] args) {
+        Student student1 = new Student() ;
+        student1.setName("QianQian Huang") ;
+        System.out.println(student1.getName()) ;
+    }
+}
